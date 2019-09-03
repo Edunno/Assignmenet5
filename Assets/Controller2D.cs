5c7d193eae32fef63862c98650cab42a0681a024
Edunno <tamericandream@hotmail.com>
2019-09-03 13:39:56 +0200

    Start

---
+++ Assets/Controller2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour
+{
+    private LayerMask environmentLayerMask;
+    void Start()
+    {
+        environmentLayerMask = LayerMask.GetMask("Environment");
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) && IsGrounded()){
+            Vector2 v = new Vector2(0,200);
+            this.GetComponent<Rigidbody2D>().AddForce(v);
+        }
+    }
+    bool IsGrounded(){
+        Vector2 pos = new Vector2(transform.position.x,transform.position.y);
+        return Physics2D.Raycast(pos, Vector2.down, GetComponent<BoxCollider2D>().size.y,environmentLayerMask);
+    }
+}
